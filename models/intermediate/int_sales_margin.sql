2f0b96d6e301f7fcb396a30cf1af3dcfe01365f5
taifaya38@gmail.com <taifaya38@gmail.com>
2025-09-30 10:17:16 +0000

    creation of a finance  days section

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,9 +1,12 @@
- select 
-        date_date,
-        revenue,
-        quantity,
-        round(revenue-(quantity * purchase_price),2) as margin,
-        round(quantity * purchase_price,2) as purchase_cost
-    from {{ ref('stg_raw__sales') }}  as sales
-    inner join {{ ref('stg_raw__product') }} as products
-    on sales.product_id=products.products_id
+ SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      ON s.product_id=p.products_id
