2fc57b84eb2116c1a4717eb4e87fb6db1dcd2b53
taifaya38@gmail.com <taifaya38@gmail.com>
2025-09-30 09:03:43 +0000

    creation intermediate model

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,6 +1,13 @@
- select sales.orders_id,
-        *
-    from {{ ref('int_sales_margin') }} as margin
-    inner join {{ ref('stg_raw__sales') }}  as sales
-   using (date_date)
+ select  
+     sales.orders_id,
+     date_date,
+     ROUND(SUM(revenue),2) as revenue,
+     ROUND(SUM(quantity),2) as quantity,
+     ROUND(SUM(purchase_cost),2) as purchase_cost,
+     ROUND(SUM(margin),2) as margin
+     FROM {{ ref("int_sales_margin") }}
+     inner join {{ ref('stg_raw__sales') }}  as sales
+     using (date_date)
+     GROUP BY sales.orders_id,date_date
+     ORDER BY sales.orders_id DESC
 
